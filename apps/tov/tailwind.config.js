9c20f9fb4d0d96c1c94d7d6fe5e7e02f4b7b36c5
박진용 <chesedi@hanatour.com>
2023-03-03 16:24:48 +0900

    [Tov] tailwind 설정 파일 추가

---
+++ apps/tov/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // content: [
+  //   `@/components/**/*.{vue,js,ts}`,
+  //   `@/layouts/**/*.vue`,
+  //   `@/pages/**/*.vue`,
+  //   `@/composables/**/*.{js,ts}`,
+  //   `@/plugins/**/*.{js,ts}`,
+  //   `@/App.{js,ts,vue}`,
+  //   `@/app.{js,ts,vue}`,
+  //   `@/Error.{js,ts,vue}`,
+  //   `@/error.{js,ts,vue}`,
+  // ],
+  darkMode: 'class',
+  variants: {},
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
