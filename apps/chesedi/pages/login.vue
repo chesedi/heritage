8e802fcbff7f26d85a444605b941ed7bb0814b28
박진용 <chesedi@hanatour.com>
2023-03-09 16:35:09 +0900

    [chesedi] 로그인 & 회원가입 UI 수정

--- apps/chesedi/pages/login.vue
+++ apps/chesedi/pages/login.vue
@@ -22,56 +22,86 @@ watchEffect(async () => {
 })
 </script>
 <template>
-  <div class="flex item-center justify-center">
-    <!--  <form>-->
-    <!--    <div>-->
-    <!--      <label for="email">Email</label>-->
-    <!--      <input type="email" id="email" v-model="credentials.email" />-->
-    <!--    </div>-->
-    <!--    <div>-->
-    <!--      <label for="password">Password</label>-->
-    <!--      <input type="password" id="password" v-model="credentials.password" />-->
-    <!--    </div>-->
-    <!--    <div>-->
-    <!--      <button @click="login">Submit</button>-->
-    <!--    </div>-->
-    <!--  </form>-->
-    <div class="w-full max-w-xs">
-      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4 mx-auto">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="username"> Email </label>
-          <input
-            v-model="credentials.email"
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
-            type="text"
-            placeholder="Username"
-          />
-        </div>
-        <div class="mb-6">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-            Password
-          </label>
-          <input
-            v-model="credentials.password"
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="******************"
-          />
-          <!--        <p class="text-red-500 text-xs italic">Please choose a password.</p>-->
-        </div>
-        <div class="flex items-center justify-between">
-          <button
-            @click="login"
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+  <section class="pt-10 bg-gray-50 dark:bg-gray-900">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <a
+        href="#"
+        class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+      >
+        <img
+          class="w-8 h-8 mr-2"
+          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+          alt="logo"
+        />
+        PJYSO
+      </a>
+      <div
+        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1
+            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
           >
-            Sign In
-          </button>
+            Login
+          </h1>
+          <form class="space-y-4 md:space-y-6" action="#">
+            <div class="mb-4 mx-auto">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                Email
+              </label>
+              <input
+                v-model="credentials.email"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Username"
+              />
+            </div>
+            <div class="mb-6">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                Password
+              </label>
+              <input
+                v-model="credentials.password"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="******************"
+              />
+              <!--        <p class="text-red-500 text-xs italic">Please choose a password.</p>-->
+            </div>
+            <div class="flex items-center justify-between">
+              <button
+                @click="login"
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+          <p class="text-center text-gray-500 text-xs">
+            &copy;2023 Chesedi Corp. All rights reserved.
+          </p>
         </div>
-      </form>
-      <p class="text-center text-gray-500 text-xs">&copy;2023 Chesedi Corp. All rights reserved.</p>
+      </div>
     </div>
-  </div>
+  </section>
+
+  <!--  <div class="flex item-center justify-center">-->
+  <!--  <form>-->
+  <!--    <div>-->
+  <!--      <label for="email">Email</label>-->
+  <!--      <input type="email" id="email" v-model="credentials.email" />-->
+  <!--    </div>-->
+  <!--    <div>-->
+  <!--      <label for="password">Password</label>-->
+  <!--      <input type="password" id="password" v-model="credentials.password" />-->
+  <!--    </div>-->
+  <!--    <div>-->
+  <!--      <button @click="login">Submit</button>-->
+  <!--    </div>-->
+  <!--  </form>-->
+  <!--    <div class="w-full max-w-xs"></div>-->
+  <!--  </div>-->
 </template>
--- apps/chesedi/pages/register.vue
+++ apps/chesedi/pages/register.vue
@@ -22,7 +22,7 @@ async function register() {
         surname,
         email,
       },
-      emailRedirectTo: 'http://localhost:3000/login',
+      emailRedirectTo: 'http://localhost:8415/login',
     },
   })
 
@@ -36,30 +36,119 @@ async function register() {
 </script>
 
 <template>
-  <h1>Create an account!</h1>
-  <form>
-    <div>
-      <label for="first-name">First Name</label>
-      <input type="text" id="first-name" v-model="credentials.firstName" />
+  <section class="pt-10 bg-gray-50 dark:bg-gray-900">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <a
+        href="#"
+        class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+      >
+        <img
+          class="w-8 h-8 mr-2"
+          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+          alt="logo"
+        />
+        PJYSO
+      </a>
+      <div
+        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1
+            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+          >
+            Create and account
+          </h1>
+          <form class="space-y-4 md:space-y-6" action="#">
+            <div>
+              <label
+                for="first-name"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >First Name</label
+              >
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="JinYong"
+                required=""
+                v-model="credentials.firstName"
+              />
+            </div>
+            <div>
+              <label
+                for="surname"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Surname</label
+              >
+              <input
+                type="text"
+                name="surname"
+                id="surname"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Park"
+                required=""
+                v-model="credentials.surname"
+              />
+            </div>
+            <div>
+              <label
+                for="email"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Email</label
+              >
+              <input
+                type="email"
+                name="email"
+                id="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="name@company.com"
+                required=""
+                v-model="credentials.email"
+              />
+            </div>
+            <div>
+              <label
+                for="password"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Password</label
+              >
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="••••••••"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+                v-model="credentials.password"
+              />
+            </div>
+            <div>
+              <label
+                for="confirm-password"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Confirm password</label
+              >
+              <input
+                type="password"
+                name="confirm-password"
+                id="confirm-password"
+                placeholder="••••••••"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+                v-model="credentials.passwordRepeat"
+              />
+            </div>
+            <button
+              @click="register"
+              type="submit"
+              class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Create an account
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
-    <div>
-      <label for="surname">Surname</label>
-      <input type="text" id="surname" v-model="credentials.surname" />
-    </div>
-    <div>
-      <label for="email">Email</label>
-      <input type="email" id="email" v-model="credentials.email" />
-    </div>
-    <div>
-      <label for="password">Password</label>
-      <input type="password" id="password" v-model="credentials.password" />
-    </div>
-    <div>
-      <label for="password">Repeat Password</label>
-      <input type="password" id="repeat-password" v-model="credentials.passwordRepeat" />
-    </div>
-    <div>
-      <button @click="register">Submit</button>
-    </div>
-  </form>
+  </section>
 </template>
